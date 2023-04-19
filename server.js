6d849f9e0be30b3b9cf4329ccad48d7aff21d334
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-19 08:29:24 +0100

    task4 - fetch book by id

--- server.js
+++ server.js
@@ -5,24 +5,38 @@ const server = http.createServer((req, res) => {
     const {url, method} = req;
     if (method === 'GET') {
         if (url === '/api') sendResponse(res, 'Hello world!', 'msg')
-        if (url === '/api/books') fetchAll(req, res);
-        if (url === '/api/authors') fetchAll(req, res); 
+        if (url.startsWith('/api/books')) fetchAllItems(req, res);
+        if (url.startsWith('/api/authors')) fetchAllItems(req, res); 
     }
 });
 
-const fetchAll = (req, res) => {
-    const { url } = req; // =/api/books
-    return readFile(`${__dirname}/data/${url.slice(5)}.json`, 'utf8')
-        .then((content) => sendResponse(res, JSON.parse(content), url.slice(5)))
-        .catch((err) => console.log(err))
-};
+
 
 const sendResponse = (res, parsedObj, title) => {
     res.setHeader('Content-Type', 'application/json');
     res.status = 200
     res.write(JSON.stringify({[title]: parsedObj}));
     res.end();
+};
+
+const filterItem = (res, parsedArr, id, header) => {
+    const idName = header === 'authors' ? 'authorId' : 'bookId';
+    const targetItem = parsedArr.filter((i) => i[idName] === parseInt(id));
+    sendResponse(res, targetItem, header);
 }
+
+const fetchAllItems = (req, res) => {
+    const splitUrl = req.url.split('/');
+    readFile(`${__dirname}/data/${splitUrl[2]}.json`, 'utf8')
+        .then((content) => {
+            if (splitUrl.length === 3) {
+                sendResponse(res, JSON.parse(content), splitUrl[2])
+            } else if (splitUrl.length === 4) {
+                filterItem(res, JSON.parse(content), splitUrl[3], `${splitUrl[2]}`)
+            }
+        })
+        .catch((err) => console.log(err))
+};
 
 server.listen(8000, (err) => {
     console.log(err ? err : 'Listening for requests...')
