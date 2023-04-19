72f52e6c835eb59a4daa225c5d7ddede8e35402a
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-19 07:29:06 +0100

    task 1-3 complete fetching data from local JSON

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
+const { readFile } = require('node:fs/promises');
 const http = require('node:http');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!',
-  }));
+    const {url, method} = req;
+
+    if (method === 'GET') {
+        if (url === '/api') {
+            res.setHeader('Content-Type', 'application/json');
+            res.status = 200
+            res.write(JSON.stringify({msg: 'Hello world!'}));
+            res.end();
+        }
+        if (url === '/api/books') {
+            fetchAll('data/books.json')
+                .then((data) => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status = 200
+                    res.write(JSON.stringify({books: data}));
+                    res.end();
+                })
+        }
+        if (url === '/api/authors') {
+            fetchAll('data/authors.json')
+                .then((data) => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status = 200
+                    res.write(JSON.stringify({authors: data}));
+                    res.end();
+                })
+        }
+    }
 });
 
-server.listen(8000);
+const fetchAll= (dbPath) => {
+    return readFile(`${__dirname}/${dbPath}`, 'utf8')
+        .then((res) => JSON.parse(res))
+        .catch((err) => console.log(err))
+};
+
+
+server.listen(8000, (err) => {
+    console.log(err ? err : 'Listening for requests...')
+});
